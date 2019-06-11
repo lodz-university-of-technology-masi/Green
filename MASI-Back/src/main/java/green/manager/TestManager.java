51a9d77ec8fe59d4c52ed67bc39cc7111e50fdba
Filip Pietrusiak <189948@edu.p.lodz.pl>
2019-06-11 11:48:06 +0200

    First test implementation.

---
+++ MASI-Back/src/main/java/green/manager/TestManager.java
@@ -0,0 +1,72 @@
+package green.manager;
+
+import green.entity.Test;
+import green.model.response.GetAllResponse;
+import green.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TestManager {
+
+    @Autowired
+    TestRepository testRepository;
+
+    public ResponseEntity<GetAllResponse<Test>> getAllTests()
+    {
+        GetAllResponse body = new GetAllResponse();
+        body.setMessage("OK");
+        body.setList(testRepository.findAll());
+        return new ResponseEntity(body, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Test> createTest(Test test)
+    {
+        test = testRepository.save(test);
+        return new ResponseEntity(test, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Test> updateTest(Test test)
+    {
+        Optional<Test> old = testRepository.findById(test.getId());
+        if(old.isPresent())
+        {
+            return createTest(test);
+        }
+        else
+        {
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<Test> getTest(Integer id)
+    {
+        Optional<Test> test = testRepository.findById(id);
+        if(test.isPresent())
+        {
+            return new ResponseEntity(test.get(), HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<String> deleteTest(Integer id)
+    {
+        Optional<Test> test = testRepository.findById(id);
+        if(test.isPresent())
+        {
+            testRepository.deleteById(id);
+            return new ResponseEntity("OK", HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity("This position does not exists!", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
